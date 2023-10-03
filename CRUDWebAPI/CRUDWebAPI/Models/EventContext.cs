69201359529de2e4f6834b40c4f57016e8ba4e37
t3rixis <spons0rr1703@gmail.com>
2023-10-03 23:57:43 +0300

    Feat: added migration

--- CRUDWebAPI/CRUDWebAPI/Models/EventContext.cs
+++ CRUDWebAPI/CRUDWebAPI/Models/EventContext.cs
@@ -1,6 +1,13 @@
-﻿namespace CRUDWebAPI.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDWebAPI.Models
 {
-    public class EventContext
+    public class EventContext : DbContext
     {
+        public EventContext(DbContextOptions<EventContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Event> Events { get; set; }
     }
 }
--- CRUDWebAPI/CRUDWebAPI/Program.cs
+++ CRUDWebAPI/CRUDWebAPI/Program.cs
@@ -1,7 +1,11 @@
+using CRUDWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<EventContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EventCS")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
